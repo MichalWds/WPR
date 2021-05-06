7f870a87cb126ef66cd217c2e2ae7ff2e15f23d2
Wadas <mawd@gft.com>
2021-05-06 09:37:57 +0200

    test

---
+++ 3/3_1_2.php
@@ -0,0 +1,20 @@
+<?php
+function addition($firstNumber, $secondNumber)
+{
+    echo $firstNumber + $secondNumber;
+}
+
+function subtraction($firstNumber, $secondNumber)
+{
+    echo $firstNumber - $secondNumber;
+}
+
+function multiplication($firstNumber, $secondNumber)
+{
+    echo $firstNumber * $secondNumber;
+}
+
+function division($firstNumber, $secondNumber)
+{
+    echo $firstNumber / $secondNumber;
+}
